3095f16823285573c6dac4782dd6f83b2db2c61a
sofiacurbeloo <sofiacurbelo923@gmail.com>
2024-11-24 19:05:38 -0300

    get-por-id

--- Back-end/modules/modules.js
+++ Back-end/modules/modules.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
+//Obtener datos de todos lo json
 const readApiData = async (dir) => {
     const files = fs.readdirSync(dir); //lee la ruta y la guarda
     let data = []; //lista donde se van a cargar los json a mostrar
@@ -15,10 +16,10 @@ const readApiData = async (dir) => {
     return data;
 };
 
+//Obtener dato de json por id
 const readApiDataById = async (file) =>{
     const fileContent = fs.readFileSync(file, 'utf8');
     return JSON.parse(fileContent);
-  
 }
   
 
